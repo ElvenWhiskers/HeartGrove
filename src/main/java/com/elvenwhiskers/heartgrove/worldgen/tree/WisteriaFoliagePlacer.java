a8a5429a4dec8a142ed94b96a0ddfe99aa8a7496
ElvenWhiskers <Bringel.Brittany@Gmail.com>
2026-09-02 08:29:54 -0700

    Changed tree placer to grow vines properly.

--- src/main/java/com/elvenwhiskers/heartgrove/worldgen/tree/WisteriaFoliagePlacer.java
+++ src/main/java/com/elvenwhiskers/heartgrove/worldgen/tree/WisteriaFoliagePlacer.java
@@ -10,6 +10,9 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import com.elvenwhiskers.heartgrove.block.ModBlocks;
+import com.elvenwhiskers.heartgrove.block.custom.WisteriaVineBlock;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WisteriaFoliagePlacer extends FoliagePlacer{
 
@@ -34,6 +37,8 @@ public class WisteriaFoliagePlacer extends FoliagePlacer{
         BlockPos center = attachment.pos(); //The spot where the leaves attach to the trunk
         BlockState transitionLeaves = ModBlocks.BLUE_WISTERIA_LEAVES.get().defaultBlockState();
         BlockState blossoms = ModBlocks.BLUE_WISTERIA_BLOSSOMS.get().defaultBlockState();
+        List<BlockPos> lowerBlossomPositions = new ArrayList<>();
+
 
         for (int z = -1; z <= 1; z++) { //top layer - Y + 1 - all green
             for (int x = -1; x <= 1; x++) {
@@ -67,10 +72,21 @@ public class WisteriaFoliagePlacer extends FoliagePlacer{
             }
         }
 
-        foliageSetter.set(center.offset(-2, -1, -2), blossoms);
-        foliageSetter.set(center.offset(2, -1, -2), blossoms);
-        foliageSetter.set(center.offset(-2, -1, 2), blossoms);
-        foliageSetter.set(center.offset(2, -1, 2), blossoms);
+        //extra pieces
+        BlockPos layer3NorthWest = center.offset(-2, -1, -2);
+        BlockPos layer3NorthEast = center.offset(2, -1, -2);
+        BlockPos layer3SouthWest = center.offset(-2, -1, 2);
+        BlockPos layer3SouthEast = center.offset(2, -1, 2);
+
+        foliageSetter.set(layer3NorthWest, blossoms);
+        foliageSetter.set(layer3NorthEast, blossoms);
+        foliageSetter.set(layer3SouthWest, blossoms);
+        foliageSetter.set(layer3SouthEast, blossoms);
+
+        lowerBlossomPositions.add(layer3NorthWest);
+        lowerBlossomPositions.add(layer3NorthEast);
+        lowerBlossomPositions.add(layer3SouthWest);
+        lowerBlossomPositions.add(layer3SouthEast);
 
         //4th layer - y-2 - full color
         for (int z = -1; z <= 1; z++) {
@@ -82,6 +98,7 @@ public class WisteriaFoliagePlacer extends FoliagePlacer{
 
                 BlockPos leafPos = center.offset(x, -2, z);
                 foliageSetter.set(leafPos, blossoms);
+                lowerBlossomPositions.add(leafPos);
             }
         }
 
@@ -95,20 +112,72 @@ public class WisteriaFoliagePlacer extends FoliagePlacer{
             for (int x = -1; x <= 1; x++) {
                 BlockPos leafPos = center.offset(x, -2, z);
                 foliageSetter.set(leafPos, blossoms);
+                lowerBlossomPositions.add(leafPos);
             }
         }
 
         //Fifth layer - Y -3 - blossoms - outer tips
-        foliageSetter.set(center.offset(0, -3, -3), blossoms);
-        foliageSetter.set(center.offset(-3, -3, 0), blossoms);
-        foliageSetter.set(center.offset(3, -3, 0), blossoms);
-        foliageSetter.set(center.offset(0, -3, 3), blossoms);
+        BlockPos outerNorth = center.offset(0, -3, -3);
+        BlockPos outerWest = center.offset(-3, -3, 0);
+        BlockPos outerEast = center.offset(3, -3, 0);
+        BlockPos outerSouth = center.offset(0, -3, 3);
+
+        foliageSetter.set(outerNorth, blossoms);
+        foliageSetter.set(outerWest, blossoms);
+        foliageSetter.set(outerEast, blossoms);
+        foliageSetter.set(outerSouth, blossoms);
+
+        lowerBlossomPositions.add(outerNorth);
+        lowerBlossomPositions.add(outerWest);
+        lowerBlossomPositions.add(outerEast);
+        lowerBlossomPositions.add(outerSouth);
 
         //Fifth layer - Y -3 - blossoms - inner pieces
-        foliageSetter.set(center.offset(-1, -3, -1), blossoms);
-        foliageSetter.set(center.offset(1, -3, -1), blossoms);
-        foliageSetter.set(center.offset(-1, -3, 1), blossoms);
-        foliageSetter.set(center.offset(1, -3, 1), blossoms);
+        BlockPos innerNorthWest = center.offset(-1, -3, -1);
+        BlockPos innerNorthEast = center.offset(1, -3, -1);
+        BlockPos innerSouthWest = center.offset(-1, -3, 1);
+        BlockPos innerSouthEast = center.offset(1, -3, 1);
+
+        foliageSetter.set(innerNorthWest, blossoms);
+        foliageSetter.set(innerNorthEast, blossoms);
+        foliageSetter.set(innerSouthWest, blossoms);
+        foliageSetter.set(innerSouthEast, blossoms);
+
+        lowerBlossomPositions.add(innerNorthWest);
+        lowerBlossomPositions.add(innerNorthEast);
+        lowerBlossomPositions.add(innerSouthWest);
+        lowerBlossomPositions.add(innerSouthEast);
+
+        //vine layer
+        // Check every lower-canopy blossom.
+        // If there is no blossom directly beneath it,
+        // it is part of the exposed underside of the canopy.
+        for (BlockPos blossomPos : lowerBlossomPositions) {
+
+            BlockPos vinePos = blossomPos.below();
+
+            if (lowerBlossomPositions.contains(vinePos)) {
+                continue;
+            }
+
+            // 50% chance for an exposed blossom to start a vine.
+            if (random.nextFloat() >= 0.5F) {
+                continue;
+            }
+
+            BlockState vineState =
+                    ModBlocks.BLUE_WISTERIA_VINES.get()
+                            .defaultBlockState()
+                            .setValue(WisteriaVineBlock.BOTTOM, true)
+                            .setValue(
+                                    WisteriaVineBlock.GROWTH_REMAINING,
+                                    random.nextInt(7)
+                            );
+
+            foliageSetter.set(vinePos, vineState);
+        }
+
+
 
     }
 
